e10c20218422c3fee1609c2a0172a40e8e8a07e4
Jaibhev Dhiman <legendaryjd30@gmail.com>
2026-08-24 10:24:35 +0000

    Balance Parenthesis

---
+++ Assignments/week_7/Bal_paranthesis.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class Main {
+    public static boolean isBalanced(String expression) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : expression.toCharArray()) {
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+
+   
+            else if (ch == ')' || ch == ']' || ch == '}') {
+
+                // No matching opening bracket
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == ']' && top != '[') ||
+                    (ch == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("[()]{}{[()()]}")); // true
+        System.out.println(isBalanced("[()"));             // false
+    }
+}
