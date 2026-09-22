b2237a9e6f904806a6f9e0b9a224e283c1e924df
Jaibhev Dhiman <legendaryjd30@gmail.com>
2026-09-22 13:52:13 +0000

    Reverse Linkedlist using recursion

---
+++ Assignments/week_10/reverseLL.java
@@ -0,0 +1,59 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class reverseLL {
+
+    public static ListNode reverseList(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+       
+        head.next.next = head;
+
+        head.next = null;
+
+        return newHead;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Creating: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
